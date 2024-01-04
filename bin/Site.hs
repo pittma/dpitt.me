fead3455dabf15b3b1605b5ff1d507f4fc9d5528
dan pittman <dan@dpitt.me>
2024-01-04 04:50:14 -0800

    fill in index page, add talks and cv

--- bin/Site.hs
+++ bin/Site.hs
@@ -25,6 +25,9 @@ baseRules = do
   match "js/*.js" $ do
     route idRoute
     compile copyFileCompiler
+  match "files/*" $ do
+    route idRoute
+    compile copyFileCompiler
   match "images/*" $ do
     route idRoute
     compile copyFileCompiler
